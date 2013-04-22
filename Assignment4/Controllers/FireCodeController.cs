33cf793e69dfa565669f05e7581692312ae4537d
JMagashazi <joshua.magashazi@gmail.com>
2013-04-21 22:25:02 -0400

    Get it done

---
+++ Assignment4/Controllers/FireCodeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment4.Controllers
+{
+    public class FireCodeController : ApiController
+    {
+
+        const string TRUEFIRECODE = "AZ035T29Z1984";
+
+        public string Get(string fireCode)
+        {
+            if (fireCode == TRUEFIRECODE)
+            {
+                return "AUTHENTICATED";
+            }
+
+            return "DRILL";
+        }
+    }
+}
